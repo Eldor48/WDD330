533cac746350f289b7322a906940629d8a748a56
Enmanuel De los Santos Cruz <41967722+Eldor48@users.noreply.github.com>
2022-10-02 00:12:59 -0400

    week 3

---
+++ week 2/week_2_notes.js
@@ -0,0 +1,28 @@
+// this is a short comment
+/* This is a longer comment
+anything here will be ignored
+This is a useful place to put notes
+*/
+
+const message = 'Hello, world!';
+console.log(message);
+/*
+Reserved words are: 
+abstract, await, boolean, break, byte, case, catch, char, class, const, continue, debugger, default, delete, do, double, else, enum, export, extends, false, final, finally, float, for, function, goto, if, implements, import, in instanceof, int, interface, let, long, native, new, null, package, private, protected, public, return, short, static, super, switch, synchronized, this, throw, throws, transient, true, try, typeof, var, volatile, void, while, with, yield
+*/ 
+
+console.log(typeof 5)
+{  number = 5}
+
+
+console.log(number)
+
+biora = new String('number') 
+
+console.log(biora)
+
+const surname = 'Alexaaaaaaaaaaaaaaaaaaaaa'
+
+console.log(surname.includes('k'))
+const brother = `Victor`
+console.log(`Hello${brother}`)
